d6a4536d13500d6a64dd2cb5074f3fee683e82ca
Francisco Morosini <fmorosini@gmail.com>
2020-11-27 11:51:01 -0300

    Express básico

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo")
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
