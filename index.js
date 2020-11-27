97d9a8d554a3f3cd7bf23bf55541255aa2ea30d4
Francisco Morosini <fmorosini@gmail.com>
2020-11-27 12:34:09 -0300

    primer commit con algo

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const path = require('path');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+app.get('/test', (req,res) => {
 
+    res.json({
+        ok: true
+    })
+
+})
 
 app.listen(port, (err) => {
 
