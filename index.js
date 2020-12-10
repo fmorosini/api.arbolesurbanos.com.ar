3167196b0e86b5fecc35c87e8af796edf86a0737
Francisco Morosini <fmorosini@gmail.com>
2020-12-10 12:28:41 -0300

    cors corrige bug

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const publicPath = path.resolve(__dirname, 'public')
 
 const abreDB = () => {
