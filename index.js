a3275507e517fbccd1fedbff3e04fc79d282c3c5
Francisco Morosini <fmorosini@gmail.com>
2020-12-10 12:24:49 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('./config/config')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const pg = require('postgis-promise')({geoJSON: true})
 
 const { toGeoJSON } = require('./utils/features.js')
