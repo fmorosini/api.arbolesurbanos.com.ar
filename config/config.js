967fcdd446def8dcfd4b0bc11e4d9116f33168fb
= <=>
2021-05-06 19:46:57 -0300

    Se mejora seguridad con variables de entorno

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ let urlDesarrollo = `http://192.168.1.200:${process.env.PORT}/`
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'desarrollo'
 
-process.env.urlDB = 'postgres://postgres:kkck0303456@localhost:5432/arbolado'
+process.env.urlDB = `postgres://${process.env.PG_USR}:${process.env.PG_PWD}@localhost:5432/arbolado`
 
 process.env.urlApi = (process.env.NODE_ENV === 'desarrollo' ? urlDesarrollo : 'https://api.arbolesurbanos.com.ar/')
